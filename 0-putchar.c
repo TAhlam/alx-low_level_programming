203f2054b667bf1d0a537363228cf1e859c81ee4
TAhlam <taourirtahlam9@gmail.com>
2023-04-25 17:21:16 -0700

    task0

---
+++ 0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main _print _putchar and a new line 
+ * Return: Always to 0
+ */
+int main(void)
+{
+        _putchar('-');
+	     _putchar('c');
+	     _putchar('r');
+	     _putchar('u');
+	     _putchar('h);
+	     _putchar('t');
+	     _putchar('\n');
+	     return(0);
+}
+
